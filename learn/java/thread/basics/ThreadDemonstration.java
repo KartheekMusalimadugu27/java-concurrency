4d1f4d61f45e4f68fd03f9fffb0fcabd565459a0
Kartheek Musalimadugu <kartheekdevopspath@gmail.com>
2022-03-19 15:40:39 +0530

    Basics

--- learn/java/thread/basics/ThreadDemonstration.java
+++ learn/java/thread/basics/ThreadDemonstration.java
@@ -3,8 +3,8 @@ package learn.java.thread.basics;
 public class ThreadDemonstration {
     public static void main(String args[]) throws InterruptedException {
         SumUpExample.runTest();
-        RaceConditionThreadSafe.runTest();
-        RaceConditionThreadUnSafe.runTest();
+        //RaceConditionThreadSafe.runTest();
+        //RaceConditionThreadUnSafe.runTest();
     }
 
   
