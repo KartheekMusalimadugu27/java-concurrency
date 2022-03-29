6a2b039dc6c2620f8076d1770118f749f8776fd0
Kartheek Musalimadugu <kartheekdevopspath@gmail.com>
2022-03-29 13:27:46 +0530

    concurrency basics

---
+++ learn/java/callablefuture/FutureCallableDemo.java
@@ -0,0 +1,37 @@
+package learn.java.callablefuture;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FutureCallableDemo {
+
+    static ExecutorService threadPool = Executors.newFixedThreadPool(2);
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println( "sum :" + findSum(10));
+        threadPool.shutdown();
+    }
+
+    static int findSum(final int n) throws ExecutionException, InterruptedException{
+        
+        Callable<Integer> sumTask = new Callable<Integer>() {
+
+            @Override
+            public Integer call() throws Exception {
+                int sum = 0;
+                for (int i = 1; i <= n; i++)
+                    sum += i;
+                return sum;
+            }
+            
+        };
+
+        Future<Integer> f = threadPool.submit(sumTask);
+        return f.get();
+    }
+    
+}
